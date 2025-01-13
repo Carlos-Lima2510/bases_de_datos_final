4b9a5f3ad5b616f1ae5b9f12193805ca6a63ee5c
Carlos-Lima2510 <carlos251005.alvarado@gmail.com>
2025-01-13 15:28:56 +0100

    consultas personales

--- consultas.sql
+++ consultas.sql
@@ -9,6 +9,21 @@ BEGIN
 -- Consultas Carlos Alvarado --
 
 
+-- Intersección --
+
+SELECT * FROM funcion WHERE horario > '2025-01-01 00:00:00' INTERSECT SELECT * FROM funcion WHERE codigo_funcion LIKE 'AB%';
+
+-- Diferencia --
+
+SELECT * FROM funcion WHERE horario > '2025-01-01 00:00:00' EXCEPT SELECT * FROM funcion WHERE codigo_funcion = 'AB-103';
+
+-- Unión --
+
+SELECT * FROM funcion WHERE sala_id = 1 UNION SELECT * FROM funcion WHERE codigo_funcion = 'AB-103';
+
+-- Agregación --
+
+SELECT MAX(horario) FROM funcion;
 
 END //
 
